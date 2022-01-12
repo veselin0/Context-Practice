fd4b55ae62aeed94a40d5d225b9db648843280c1
Veselin <vesselin60@gmail.com>
2022-01-12 18:00:17 +0200

    Context Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import UserContext from "./userContext";
+import Header from "./Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+        <Header />
+        <main>
+            <UserContext.Consumer>
+                {username => (
+                    <p className="main">No new notifications, {username}! 🎉</p>
+                )}
+            </UserContext.Consumer>
+        </main>
+        
+        {
+            /**
+             * Challenge: Add the ability for the user to choose a new username
+             * 
+             * 1. Add state to this component to hold the new username in a controlled form
+             * (https://reactjs.org/docs/forms.html#controlled-components)
+             * (https://scrimba.com/p/p7P5Hd/cW8Jdfy)
+             * 
+             * 2. Change userContext into a component that has state and provides the ability
+             * to change the user's username. Make sure to export the provider and consumer
+             * as named exports and update their uses anywhere else in the app
+             * 
+             * 3. Give this App component the ability to update the username in context when the
+             * button is clicked
+             */
+        }
+        <input
+            type="text"
+            name="username"
+            placeholder="New username"
+            // value={}
+            // onChange={}
+        />
+        <button 
+        //onClick={}
         >
-          Learn React
-        </a>
-      </header>
+          Change Username</button>
+        
     </div>
-  );
+)
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import UserContext from "./userContext"
+
+function Header() {
+    return (
+        <header>
+            <UserContext.Consumer>
+                {username => (
+                    <p>Welcome, {username}!</p>
+                )}
+            </UserContext.Consumer>
+        </header>
+    )
+}
+
+export default Header
